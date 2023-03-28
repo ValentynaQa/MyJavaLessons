85c4ab9a9b1ccd2fd3391878d6f475288bb9333d
ТехноРАЙ <10vavaan@gmail.com>
2023-03-28 15:34:37 +0300

    HomeWork_4

--- home_work_4/src/Main.java
+++ home_work_4/src/Main.java
@@ -16,39 +16,4 @@ public class Main {
 
     }
 }
-    /*public static void main(String[] args) {
-        // Создаем несколько семей
-        Human father1 = new Human("John", "Doe", 1975, new String[][]{{"Monday", "go to work"}, {"Tuesday", "go to work"}});
-        Human mother1 = new Human("Jane", "Doe", 1978);
-        Human child1 = new Human("Billy", "Doe", 2005, new String[][]{{"Monday", "go to school"}, {"Tuesday", "go to school"}});
-        Pet pet1 = new Pet("Dog", "Rex", 3, 75, new String[]{"eat", "sleep", "play"});
 
-        Human father2 = new Human("Mike", "Smith", 1980);
-        Human mother2 = new Human("Lisa", "Smith", 1982, new String[][]{{"Monday", "go to work"}, {"Tuesday", "go to work"}});
-        Human child2 = new Human("Tom", "Smith", 2008, new String[][]{{"Monday", "go to school"}, {"Tuesday", "go to school"}});
-        Pet pet2 = new Pet("Cat", "Fluffy", 2, 60, new String[]{"sleep", "play"});
-
-        // Выводим информацию о каждом человеке
-        System.out.println(father1);
-        System.out.println(mother1);
-        System.out.println(child1);
-        System.out.println(father2);
-        System.out.println(mother2);
-        System.out.println(child2);
-
-        // Создаем семью
-        Family family = new Family(father1, mother1);
-        family.addChild(child1);
-
-        // Присваиваем ссылки
-        child1.setFamily(family);
-        child1.setPet(pet1);
-
-        // Вызываем методы у ребенка и его питомца
-        child1.greetPet();
-        child1.describePet();
-        child1.eatPet();
-        System.out.println(child1.toString());
-        System.out.println(pet1.toString());
-    }
-}*/
